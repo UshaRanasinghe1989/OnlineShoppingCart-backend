40f0f9a305293e4486d19bd92c32fd2d4599076f
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2024-10-26 18:05:46 +0530

    ProductService interface created

---
+++ ShoppingCart/src/main/java/edu/ex/shoppingcart/service/ProductService.java
@@ -0,0 +1,34 @@
+package edu.ex.shoppingcart.service;
+
+import edu.ex.shoppingcart.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    //SAVE NEW PRODUCT
+    void persist(Product product);
+
+    //FIND ALL PRODUCTS
+    List<Product> findAll();
+
+    //UPDATE AN EXISTING PRODUCT
+    void update(Product product, Long id);
+
+    //DELETE AN EXISTING PRODUCT
+    void delete(Long id);
+
+    //FIND A PRODUCT BY ID
+    Product findById(Long id);
+
+    //FIND BY PRODUCT NAME
+    List<Product> findByProductName(String productName);
+
+    //FIND PRODUCTS BY BRAND NAME
+    List<Product> findByBrandName(String brandName);
+
+    //FIND PRODUCTS BY CATEGORY NAME
+    List<Product> findByCategoryName(String categoryName);
+
+    //FIND PRODUCTS BY BRAND AND CATEGORY
+    List<Product> findByBrandAndCategory(String brand, String category);
+}
